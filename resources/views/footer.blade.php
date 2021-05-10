a13c23824fd98e59a9c5757e7dc589d4273df0c5
Daniel Esparza <danielesparzza@gmail.com>
2021-05-10 15:14:53 -0500

    Inicio del repositorio

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,48 @@
+ <!-- FOOTER -->
+
+ <footer id="footer" class="pb-4 pt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg">
+                <div class="row">
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <h5>Contacto</h5>
+                    </div>
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <small>5 de mayo #545</small>
+                    </div>
+                    <div class="col-12 col-lg-12 col-med-12">
+                        <small>Tel: 4746334648</small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-lg">
+                <a href="#">Preguntas frecuentes</a>
+            </div>
+
+            <div class="col-12 col-lg">
+                <a href="#">Terminos y condiciones</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<footer>
+
+    <div id="footerDivider" class="pt-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <p>Copyright 2021 TecnoEduca</p>
+                </div>
+                <div class="col-lg-6 col-12 text-right">
+                    <p>Noticias de privacidad</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+<!-- END FOOTER -->
